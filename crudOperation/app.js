1149021bc8f529f810ecc4f186b0ca2620a30fbe
suraj-sadare <sadaresuraj@gmail.com>
2021-05-14 09:32:43 +0530

    I am able to do update operation

--- crudOperation/app.js
+++ crudOperation/app.js
@@ -16,7 +16,7 @@ app.post("/articales",(req,res)=>{
 });
 app.get("/articales", async(req,res)=>{
     try{
-    const user= await artiCollection.find()
+    const user= await artiCollection.find();
    res.json(user);
     }
     catch(err){
@@ -24,5 +24,31 @@ app.get("/articales", async(req,res)=>{
     }
 
 });
+app.patch("/:id",async(req,res)=>{
+    try{
+        const user=await artiCollection.findById(req.params.id);
+        user.name=req.body.name;
+        user.author=req.body.author;
+        user.desc=req.body.desc;
+        const update=await user.save();
+        res.json(update);
+    }catch(err){
+        res.send('error');
+    }
+})
+app.delete("/:id",async(req,res)=>{
+    try{
+        await artiCollection.findByIdAndRemove(req.params.id).then(()=>{
+            const user= artiCollection.find();
+            const update= user.save();
+            res.json(update);
+        })
+     
+        
+    
+    }catch(err){
+        res.send('error');
+    }
+})
 
 app.listen(port,()=>console.log(`sending to port ${port}`));
